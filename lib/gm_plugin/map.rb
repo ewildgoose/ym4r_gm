4036a30d234b2c82b11857b437d85ab7606cba20
erbmicha <erbmicha@c9bc9743-7714-0410-b404-d8b1d4e1ace9>
2009-03-31 15:42:03 +0000

    updated map.rb to use ActionController::Base.relative_url_root instead of ActionController::AbstractRequest.relative_url_root per Rails 2.2+ (https://rails.lighthouseapp.com/projects/8994/tickets/1154-deprecated-method-of-setting-relative_url_root-inside-railsconfigurator)
    git-svn-id: http://ym4r.rubyforge.org/svn/Plugins/GM/trunk/ym4r_gm@107 c9bc9743-7714-0410-b404-d8b1d4e1ace9

--- lib/gm_plugin/map.rb
+++ lib/gm_plugin/map.rb
@@ -32,7 +32,7 @@ module Ym4r
         options[:local_search] = false unless options.has_key?(:local_search)
         api_key = ApiKey.get(options)
         a = "<script src=\"http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=#{api_key}&amp;hl=#{options[:hl]}\" type=\"text/javascript\"></script>\n"
-        a << "<script src=\"#{ActionController::AbstractRequest.relative_url_root}/javascripts/ym4r-gm.js\" type=\"text/javascript\"></script>\n" unless options[:without_js]
+        a << "<script src=\"#{ActionController::Base.relative_url_root}/javascripts/ym4r-gm.js\" type=\"text/javascript\"></script>\n" unless options[:without_js]
         a << "<style type=\"text/css\">\n v\:* { behavior:url(#default#VML);}\n</style>" if options[:with_vml]
         a << "<script src=\"http://www.google.com/uds/api?file=uds.js&amp;v=1.0\" type=\"text/javascript\"></script>" if options[:local_search]
         a << "<script src=\"http://www.google.com/uds/solutions/localsearch/gmlocalsearch.js\" type=\"text/javascript\"></script>\n" if options[:local_search]
